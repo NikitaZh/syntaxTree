ae22c70bc340813a2c97cb7db6472a4a5191dd1d
Nikita Zhadaev <zhadaevnikita@mail.ru>
2016-05-22 01:32:17 +0300

    next

---
+++ path_to_texts.py
@@ -0,0 +1,58 @@
+# -*- coding: utf8 -*-
+
+def get_doc_ids(txt_file):
+    '''Достает из документа с айди массив где первый элемент айди, второй - название документа'''
+    doc = open(txt_file,'r', encoding='utf-8')
+    arr = []
+    i = False
+    for line in doc:
+        line = line.split()
+        if i is False:
+            i = True
+        else:
+            arr.append(line[:2])
+    doc.close()
+    return arr
+
+def get_ids_text(groups_text, id):
+    '''Достает из текста с чейнами только нужные строчки, соответствующие тексту(по айди)'''
+    t = open(groups_text, 'r', encoding='utf-8')
+    id_text = []
+    for line in t:
+        line = line.split('\t')
+        if line[0] == str(id):
+            id_text.append(line)
+    t.close()
+    return id_text
+
+def get_chain(chains_arr, numb):
+    chain = []
+    for line in chains_arr:
+        if line[3] == numb:
+            chain.append([line[7], line[5]])
+    return chain
+
+def get_coref_chains(chains_arr):
+    '''Из массива с чейнами достает местоимения и их цепочки'''
+    pron_chains = []
+    for line in chains_arr:
+        if 'str:pron' in line[9]:
+            chain = get_chain(chains_arr, line[3])
+            pron_chains.append([line[7], chain])
+        elif 'str:refl' in line[9]:
+            chain = get_chain(chains_arr, line[3])
+            pron_chains.append([line[7], chain])
+        elif 'str:rel' in line[9]:
+            chain = get_chain(chains_arr, line[3])
+            pron_chains.append([line[7], chain])
+    return pron_chains
+
+
+
+
+
+
+
+p = get_ids_text('Groups.txt', 5)
+ch = get_coref_chains(p)
+print(ch)
--- saText.py
+++ saText.py
@@ -1,5 +1,5 @@
 # -*- coding: utf8 -*-
-
+from path_to_texts import get_ids_text
 
 def parse_saText(conll_text):
     """Parsing syntax-annotated text(.conll) for sentences"""
@@ -7,6 +7,8 @@ def parse_saText(conll_text):
     saText = open(conll_text, 'r', encoding='utf-8')
     for line in saText:
         if line != '\n':
+            line = line.strip('\n')
+            line = line.split('\t')
             sentences[-1].append(line)
         else:
             sentences.append([])
@@ -14,23 +16,61 @@ def parse_saText(conll_text):
     saText.close()
     return sentences
 
+def get_shifts(text):
+    #print(text)
+    arr = []
+    for line in text:
+        l = []
+        l.append(line[1])
+        l.append(line[3])
+        arr.append(l)
+    return arr
+
+
+def get_shift_info(sentences, shifts):
+    sh = 0
+
+    for sent in sentences:
+        for word in sent:
+            i = 0
+            for q in range(len(shifts)):
+                #print(shifts[q])
+                if word[1] == shifts[q][1]:
+                    word.append(shifts[q][0])
+                    shifts = shifts[q:]
+                    break
+                else:
+                    if i < 5:
+                        i += 1
+                    else:
+                        word.append('000')
+                        break
+    return sentences
+
+
+
+
+
+
+
 def pronoun_snips(parsed_text):
     """Finding pronoun and its context"""
-    pronouns = ['он', 'она', 'оно', 'они', 'я', 'свой', 'мой', 'себе', 'который']
+    pronouns = ['он', 'она', 'оно', 'они', 'его', 'её', 'их', 'свой', 'мой', 'себя', 'который']
     snips = []
-    count = 0
+    #distance = 0
     for i in range(len(parsed_text)):
         for word in parsed_text[i]:
-            syntax_info = word.split('\t')
+            #distance += 1
+            syntax_info = word
+            #print(syntax_info[1], distance)
             if syntax_info[2] in pronouns:
-                #print(syntax_info[0],syntax_info[2])
-                count += 1
+                #distance += 1
                 if i-3 >= 0:
                     snips.append(parsed_text[i-3:i+1])
                 else:
                     snips.append(parsed_text[:i+1])
                 snips[-1].append(syntax_info[0])
-    print(count)
+    #print(distance)
     if len(snips) > 0:
         return snips
     """возвращает массив, в котором каждый элемент содержит контекст местоимений
@@ -42,12 +82,10 @@ def pronoun_snips(parsed_text):
 
 
 #t = parse_saText('2_astafiev_zhizn_prozhit.conll')
+#q = get_shift_info(t, get_shifts(get_ids_text('Tokens.txt', 5)))
+
 #p = pronoun_snips(t)
-
-#for q in p:
-    #print(q)
-
-#print(p[0], p[1])
+#print(p[30])
 
 """for word in t[0]:
     word = word.split('\t')
